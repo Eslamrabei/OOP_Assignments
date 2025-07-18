d116e3071dad05756535f6be3e28cb78eb45170b
EslamRabea <EslamRabea@DESKTOP-R7J1KN3>
2025-07-18 20:44:34 +0300

    Assignment04

---
+++ OOP_Assignment04/Classes/Circle.cs
@@ -0,0 +1,25 @@
+﻿using OOP_Assignment04.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment04.Classes
+{
+    internal class Circle : ICircle
+    {
+        decimal raduis;
+        public Circle(decimal _raduis)
+        {
+            raduis = _raduis;
+        }
+        public decimal Area { get ; set ; }
+
+        public void DisplayShapeInfo()
+        {
+            Area = (decimal)Math.PI * (raduis * raduis);
+            Console.WriteLine($"The Area of Circle : {string.Format("Price: {0:0.00}", Area)}");
+        }
+    }
+}
---
+++ OOP_Assignment04/Classes/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using OOP_Assignment04.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment04.Classes
+{
+    internal class Rectangle : IRectangle
+    {
+        decimal width;
+        decimal height;
+        public Rectangle(decimal _width , decimal _height)
+        {
+            width = _width;
+            height = _height;
+        }
+        public decimal Area { get ; set ; }
+
+        public void DisplayShapeInfo()
+        {
+            Area = width * height;
+            Console.WriteLine($"The Rectangle of Circle : {string.Format("{0:0.00}",Area)}");
+
+        }
+    }
+}
